376f45e2e76242c7a379440460e0ed4f102f7e2d
c̶̢̛͕̥̯̩͎͙̼̟̠̗̮̹̜̬̥̺̺̳̣͈͔̓̅̋͑̆́̽̒̀̎̇̆͑̀̂̒͆͘̕ͅy̶̢̟̳̞̠̘̭̲̳͓̭͗͑͛̏͑̈́̓̃̋̾̎̇̓̑̕̚͜͝͝ͅk̵̮̻̼͇̜̼̳͖̮̙̙̯̐̏̀̉̃̄̏͝o̸̤̼͑͆̉̎́̀͊̑̒͗̈͛ <142172153+cykovit@users.noreply.github.com>
2024-07-20 18:33:15 +0200

    Update main.go
    update create command, update done command and add exit command

--- todogo/main.go
+++ todogo/main.go
@@ -24,61 +24,79 @@ func main() {
 	// create a new scanner to read input from keyboard
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("Commands: show create remove done clear")
+	fmt.Println("Commands: show create remove done clear exit")
 	fmt.Print("> ")
 
 	// continuously read input from user and handle the command
 	for scanner.Scan() {
 		line := scanner.Text()
-		handleInput(line, &data, &counter)
+		if handleInput(line, &data, &counter) {
+			break
+		}
 		fmt.Print("> ")
 	}
+	fmt.Println("Exiting todogo...")
 }
 
-// process the user's command and modifies the TODO list accordingly
-func handleInput(input string, data *[]TODOs, counter *int) {
-	split := strings.Split(input, " ")
+// handleInput processes the user's command and modifies the TODO list accordingly
+func handleInput(input string, data *[]TODOs, counter *int) bool {
+	split := strings.SplitN(input, " ", 2)
 	cmd := split[0]
 
+	// handle different commands based on the user's input
 	switch cmd {
 	case "create":
-		createTODO(strings.Join(split[1:], " "), data, counter)
+		if len(split) < 2 {
+			fmt.Println("Error: Missing description for create command")
+			return false
+		}
+		description := split[1]
+		createTODO(description, data, counter)
 	case "remove":
-		// ensure an ID is provided for the remove command
 		if len(split) < 2 {
 			fmt.Println("Error: Missing ID for remove command")
-			return
+			return false
 		}
 		removeTODO(split[1], data)
 	case "show":
 		// display all TODO items
 		showTODOs(data)
 	case "done":
-		// ensure an ID is provided for the done command
 		if len(split) < 2 {
-			fmt.Println("Error: Missing ID for done command")
-			return
+			fmt.Println("Error: Missing IDs for done command")
+			return false
 		}
-		// mark the TODO item with the specified ID as done
-		markTODOAsDone(split[1], data)
+		ids := split[1]
+		markTODOAsDone(ids, data)
 	case "clear":
 		clearTODOs(data)
+	case "exit":
+		return true
 	default:
 		// inform the user if the command is invalid
 		fmt.Println("Invalid command")
 	}
+	return false
 }
 
 func createTODO(description string, data *[]TODOs, counter *int) {
-	*data = append(*data, TODOs{
-		Id:          *counter,
-		Description: description,
-		Status:      false,
-	})
-	*counter++ 
-	fmt.Println("Created TODO item")
+	// split the description with commas to create multiple TODO items
+	items := strings.Split(description, ",")
+	for _, item := range items {
+		item = strings.TrimSpace(item)
+		if item != "" {
+			*data = append(*data, TODOs{
+				Id:          *counter,
+				Description: item,
+				Status:      false,
+			})
+			*counter++
+		}
+	}
+	fmt.Println("Created TODO items")
 }
 
+// remove the TODO item with the specified ID from the list
 func removeTODO(idStr string, data *[]TODOs) {
 	id, err := strconv.Atoi(idStr) // convert the ID string to an integer
 	if err != nil {
@@ -95,7 +113,7 @@ func removeTODO(idStr string, data *[]TODOs) {
 	fmt.Println("TODO item not found")
 }
 
-// showTODOs displays all TODO items in the list
+// display all TODO items in the list
 func showTODOs(data *[]TODOs) {
 	if len(*data) == 0 {
 		fmt.Println("No TODO items") // inform the user if there are no TODO items
@@ -111,24 +129,30 @@ func showTODOs(data *[]TODOs) {
 	}
 }
 
-// set the status of the TODO item with the specified ID to done
-func markTODOAsDone(idStr string, data *[]TODOs) {
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		fmt.Println("Error: Invalid ID")
-		return
-	}
-	for i, todo := range *data {
-		if todo.Id == id {
-			(*data)[i].Status = true // mark the item as done
-			fmt.Println("Marked TODO item as done")
-			return
+// set the status of the TODO items with the specified IDs to done
+func markTODOAsDone(ids string, data *[]TODOs) {
+	idStrs := strings.Split(ids, ",")
+	for _, idStr := range idStrs {
+		idStr = strings.TrimSpace(idStr)
+		if idStr == "" {
+			continue
+		}
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			fmt.Println("Error: Invalid ID", idStr)
+			continue
+		}
+		for i, todo := range *data {
+			if todo.Id == id {
+				(*data)[i].Status = true // mark the item as done
+				fmt.Println("Marked TODO item as done", id)
+				break
+			}
 		}
 	}
-	fmt.Println("TODO item not found")
 }
 
-// clearTODOs removes all TODO items from the list, clearing the slice by reinitializing it
+// remove all TODO items from the list, clear the slice by reinitializing it
 func clearTODOs(data *[]TODOs) {
 	*data = []TODOs{}
 	fmt.Println("Cleared all TODO items")
